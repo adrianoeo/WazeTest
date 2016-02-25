4065b2e1b8b91dee4e44f7effcd8e8e5ab6e1be4
adriano <adrianoeo@gmail.com>
2016-02-25 14:03:19 -0300

    commit 1

---
+++ app/src/main/java/wazetest/com/aeo/wazetest/MainActivity.java
@@ -0,0 +1,45 @@
+package wazetest.com.aeo.wazetest;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        String url = "waze://?ll=%s&navigate=yes";
+        switch (view.getId()) {
+            case R.id.btnHome:
+                url = String.format(url, "-22.812608, -47.024862");
+                break;
+            case R.id.btnWork:
+                url = String.format(url, "-22.897157, -47.101876");
+                break;
+            case R.id.btnHomeMom:
+                url = String.format(url, "-22.869306, -47.060993");
+                break;
+            case R.id.btnHomeSogra:
+                url = String.format(url, "-22.872371, -47.028865");
+                break;
+        }
+
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
+            Intent intent =
+                    new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.waze"));
+            startActivity(intent);
+        }
+
+    }
+}
