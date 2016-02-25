d184df4818c4f7f9afc7edd851467f2aa193da72
adriano <adrianoeo@gmail.com>
2016-02-25 14:38:25 -0300

    Coordinates changed

--- app/src/main/java/wazetest/com/aeo/wazetest/MainActivity.java
+++ app/src/main/java/wazetest/com/aeo/wazetest/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         String url = "waze://?ll=%s&navigate=yes";
         switch (view.getId()) {
             case R.id.btnHome:
-                url = String.format(url, "-22.812608, -47.024862");
+                url = String.format(url, "-22.812121, -47.024328");
                 break;
             case R.id.btnWork:
                 url = String.format(url, "-22.897157, -47.101876");
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
                 url = String.format(url, "-22.869306, -47.060993");
                 break;
             case R.id.btnHomeSogra:
-                url = String.format(url, "-22.872371, -47.028865");
+                url = String.format(url, "-22.874064, -47.032733");
                 break;
         }
 
